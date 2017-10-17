12dbb8dc895df2dd35f587a3736cb63d886844cb
AbdessalemOumellal <fa_oumellal@esi.dz>
2017-10-17 11:42:29 +0100

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,21 @@
+/**
+ * Created by ABDESSALEM on 13/10/2017.
+ */
+public class main {
+
+    public static void main(String args[])
+    {
+        VectorHelper tab = new VectorHelper(10);
+        tab.ShowTAB();
+        tab.MinMax();
+        tab.ShowMinMax();
+        tab.InvTAB();
+        tab.ShowTAB();
+        tab.MinMax();
+        tab.ShowMinMax();
+        tab.fonctionMult(2);
+        tab.ShowTAB();
+        tab.MinMax();
+        tab.ShowMinMax();
+    }
+}
